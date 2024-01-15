3f4869bf4b4e824bd46e20f0a40caaab87aaad57
mooochiiii <93908152+mooochiiii@users.noreply.github.com>
2024-01-15 16:26:18 +0700

    Add files via upload

---
+++ Organic shop/index.js
@@ -0,0 +1,124 @@
+// Lấy phần tử html
+
+var html = document.querySelector("html");
+
+// Lấy modal
+var modal = document.getElementById("myModal");
+
+// Lấy phần nút xoá trong modal
+var span = document.getElementsByClassName("modal-content__close")[0];
+
+// Hàm được thực thi khi người dùng click vào sản phẩm
+function ShowDetailProducts(event) {
+    // Hiển thị modal
+    modal.style = "display:block";
+    // Tắt thanh scroll bên cạnh trang web khi mở modal
+    html.style = "overflow:hidden";
+    // Lấy phần modal   
+    modal.querySelectorAll(".modal-content")[0].style = `display:block`
+    modal.querySelectorAll(".modal-content-login-form")[0].style = `display:none`
+
+
+    var e = event.target.parentElement;
+    var imgProduct = e.querySelector(".info-img").cloneNode(true);
+    imgProduct.className = "modal-content__img";
+
+    var descProduct = e.querySelector(".info__desc").cloneNode(true);
+    // descProduct.className = "modal-content__heading"
+
+    var titleProduct = e.querySelector(".info__title").cloneNode(true);
+    titleProduct.className = "modal-content__heading";
+
+    var priceProduct = e.querySelector(".info__price").cloneNode(true);
+    priceProduct = `<p class="modal-content__price">
+    <span>${priceProduct.innerHTML}</span> + Free Shipping
+    </p>`
+
+    const parser = new DOMParser();
+    let doc = parser.parseFromString(priceProduct, 'text/html');
+    priceProduct = doc.body.childNodes[0]
+    
+
+    // Replace image
+    modal
+        .querySelector(".modal-content")
+        .querySelector(".modal-wrapper")
+        .replaceChild(
+            imgProduct,
+            modal
+                .querySelector(".modal-content")
+                .querySelector(".modal-wrapper")
+                .querySelector(".modal-content__img")
+        );
+
+    // Replace title
+    modal
+        .querySelector(".modal-content")
+        .querySelector(".modal-wrapper")
+        .querySelector(".modal-wrapper__info")
+        .replaceChild(
+            titleProduct,
+            modal
+                .querySelector(".modal-content")
+                .querySelector(".modal-wrapper")
+                .querySelector(".modal-wrapper__info")
+                .querySelector(".modal-content__heading")
+        );
+
+    // Replace price
+    modal
+        .querySelector(".modal-content")
+        .querySelector(".modal-wrapper")
+        .querySelector(".modal-wrapper__info")
+        .replaceChild(
+            priceProduct,
+            modal
+                .querySelector(".modal-content")
+                .querySelector(".modal-wrapper")
+                .querySelector(".modal-wrapper__info")
+                .querySelector(".modal-content__price")
+        );
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        html.style = "overflow:visible";
+    }
+
+};
+
+
+
+
+// ====#=== Login Form ===#=== 
+// Hàm hiển thị form đăng nhập
+
+function LoginForm() {
+    modal.style = "display:flex";
+    html.style = "overflow:hidden";
+    
+    modal.querySelectorAll(".modal-content")[0].style = `display:none`
+    modal.querySelectorAll(".modal-content-login-form")[0].style = `display:block`
+
+    modal.querySelectorAll(".modal-content-login-form")[0].style = `
+    background-color: rgba(255, 255, 255, 0.8);
+    backdrop-filter: blur(10px);
+    border-radius: 10px;
+    margin: 2% auto; /* 15% from the top and centered */
+    padding: 40px 20px;
+    border: 1px solid #888;
+    width: 35%; /* Could be more or less, depending on screen size */
+    animation-name: animatetop;
+    animation-duration: 0.4s;
+    position: relative;
+    margin: auto;
+    `;
+    
+}
